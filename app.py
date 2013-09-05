2e16a8ff46737d1ed131108c8aedb68bbafa6da6
Toni Fadjukoff <lamperi@lamperi.name>
2013-09-05 21:13:41 +0300

    Fix problems with the app
    This commit fixes three problems with the application:
    - statement global lines is an error, it does nothing
    - two students don't have a focus and cols has only length of 4 so the
    app dies when it picks a wrong student
    - chooseStudents checks that it finds three different students, but it
    may return multiple same student. Fixed by using a function in python's
    random library

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 file = open('studentdata', 'r')
 lines = file.readlines()
-global lines
 file.close()
 
 @app.route("/")
@@ -25,7 +24,7 @@ def index():
 
         if (studentIndex == 0):
             featuredName = cols[0]
-            featuredFocus = cols[4]
+            featuredFocus = cols[4] if len(cols) > 4 else ''
             featured.append('featured')
         else:
             featured.append('notfeatured')
@@ -44,11 +43,7 @@ def index():
     return render_template('index.html', featuredName=featuredName, featuredFocus=featuredFocus, name1=name[order[0]], handle1=handle[order[0]], year1=year[order[0]], focus1=focus[order[0]], featured1=featured[order[0]], name2=name[order[1]], handle2=handle[order[1]], year2=year[order[1]], focus2=focus[order[1]], featured2=featured[order[1]], name3=name[order[2]], handle3=handle[order[2]], year3=year[order[2]], focus3=focus[order[2]], featured3=featured[order[2]])
 
 def chooseStudents(num=3):
-    students = []
-    while (len(set(students)) < num):
-        randomid = random.randrange(0, len(lines))
-        students.append(randomid)
-    return students
+    return random.sample(xrange(len(lines)), num)
 
 if __name__ == "__main__":
     app.run()
